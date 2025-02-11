4218e4c104d9a94fa9debf95bd3688742cb49b59
webdev.im <ttshingler@gmail.com>
2025-02-12 01:05:49 +0200

    all fixes and new loader added

---
+++ app/components/action/Loader.tsx
@@ -0,0 +1,14 @@
+// Loader.js
+
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div className="loader-container h-screen w-screen flex items-center justify-center ">
+            <span className="loader"></span>
+        </div>
+
+    );
+};
+
+export default Loader;
---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+// app/loading.tsx
+
+import Loader from './components/action/Loader'; // Adjust the import path as needed
+
+export default function Loading() {
+    return <Loader />;
+}
